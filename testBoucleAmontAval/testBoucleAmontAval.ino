fb929311b2465dcfb9d5a4628f15b9106f5dd780
valentinDubuissonPro <tintindubuisson@gmail.com>
2019-11-19 17:31:02 +0100

    Modife 19/11/2019
    Signed-off-by: valentinDubuissonPro <tintindubuisson@gmail.com>

--- testBoucleAmontAval/testBoucleAmontAval.ino
+++ testBoucleAmontAval/testBoucleAmontAval.ino
@@ -1,6 +1,8 @@
 /*Programme teste boucle AMONT AVAL
 description:test boucle amont et aval
-derniere mis a jour:13/11/2019
+date de cration:13/11/2019
+derniere mis a jour:19/11/2019:
+-Comunication avec les 2 boucles
 */
 #include <Wire.h>//Bibliothèque arduino qui permet de gerer un bus de terrain I2C
 #include "I2C.h"//Permet de paramétrer la vitesse de la liaison I2C
@@ -13,30 +15,21 @@ void setup()//fonction setup executee une fois lors du demarrage de la carte
   Wire.begin();//Ma carte Arduino devient le maitre du bus I2C
   Serial.begin(9600);//Definit la vitesse d'affichage de mon moniteur série USB 
   initI2C(122);//Cadence la vitesse de transmission
- 
 }
 
 void loop()//fonction loop executee en boucle dans le programme principal
 { 
-  Wire.beginTransmission(MECANISME);//Je commence la transmission vers esclave le PCF8574 "mecanisme"
-  Wire.requestFrom(MECANISME, 2);//demande de recevoir 2 octet a l'adresse de l'esclave
-  while(Wire.available())    // slave may send less than requested
+  Wire.beginTransmission(MECANISME);//Je commence la transmission vers l'esclave                   
+  Wire.write(120);//        P7  p6p5p4p3p2p1p0 
+                  //j'envoie 0  1 1 1 1 0 0 0  sachant que les 0 ces des sortie et 1 entré
+  Wire.endTransmission();     
+  
+  Wire.requestFrom(MECANISME, 1);//demande de recevoir 2 octet a l'adresse de l'esclave
+  while(Wire.available())//Attend la reponse
   { 
-    int a = Wire.read(); // receive a byte as character
-    Serial.print(a);// print the character
-    Serial.println("");
-    
-    int b = Wire.read();// receive a byte as character
-    Serial.print(b);// print the character
-    Serial.println("");
-    
-    int c = Wire.read();// receive a byte as character
-    Serial.print(c);// print the character
-    Serial.println("");
-    
-    int d = Wire.read();// receive a byte as character
-    Serial.print(d);// print the character
-    Serial.println("");
-    delay(1000);
+    char a = Wire.read(); //recois octet par octet
+    Serial.print(a,BIN);//affiche l'octet recus
+    Serial.println();//saut de ligne
   }
+  delay(500);
 }
